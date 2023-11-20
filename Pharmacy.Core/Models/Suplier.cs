1d10e11103595e2d8e7c39f56760bbf5ea8d6e09
mabattal <mehmetalibattal@gmail.com>
2023-11-20 12:51:46 +0300

    Entities

---
+++ Pharmacy.Core/Models/Suplier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy.Core.Models
+{
+    public class Suplier:BaseEntity
+    {
+        public string Name { get; set; }
+        public int TaxNumber { get; set; }
+        public ICollection<Medicine> Medicines { get; set; }
+    }
+}
