de3f297cf81be4da160112080e02f529967388bc
mabattal <mehmetalibattal@gmail.com>
2023-11-20 17:35:27 +0300

    IMedicineService

--- Pharmacy.API/Controllers/MedicinesController.cs
+++ Pharmacy.API/Controllers/MedicinesController.cs
@@ -12,13 +12,11 @@ namespace Pharmacy.API.Controllers
     public class MedicinesController : CustomBaseController
     {
         private readonly IMapper _mapper;
-        private readonly IService<Medicine> _service;
         private readonly IMedicineService _medicineService;
 
-        public MedicinesController(IMapper mapper, IService<Medicine> service, IMedicineService medicineService)
+        public MedicinesController(IMapper mapper, IMedicineService medicineService)
         {
             _mapper = mapper;
-            _service = service;
             _medicineService = medicineService;
         }
 
@@ -31,7 +29,7 @@ namespace Pharmacy.API.Controllers
         [HttpGet]
         public async Task<IActionResult> All()
         {
-            var medicines = await _service.GetAllAsync();
+            var medicines = await _medicineService.GetAllAsync();
             var medicinesDtos = _mapper.Map<List<MedicineDto>>(medicines.ToList());
             return CreateActionResult(CustomResponseDto<List<MedicineDto>>.Successs(200, medicinesDtos));
         }
@@ -39,7 +37,7 @@ namespace Pharmacy.API.Controllers
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(int id)
         {
-            var medicine = await _service.GetByIdAsync(id);
+            var medicine = await _medicineService.GetByIdAsync(id);
             var medicinesDto = _mapper.Map<MedicineDto>(medicine);
             return CreateActionResult(CustomResponseDto<MedicineDto>.Successs(200, medicinesDto));
         }
@@ -47,7 +45,7 @@ namespace Pharmacy.API.Controllers
         [HttpPost]
         public async Task<IActionResult> Save(MedicineDto medicineDto)
         {
-            var medicine = await _service.AddAsync(_mapper.Map<Medicine>(medicineDto));
+            var medicine = await _medicineService.AddAsync(_mapper.Map<Medicine>(medicineDto));
             var medicinesDto = _mapper.Map<MedicineDto>(medicine);
             return CreateActionResult(CustomResponseDto<MedicineDto>.Successs(201, medicinesDto));
         }
@@ -55,15 +53,15 @@ namespace Pharmacy.API.Controllers
         [HttpPut]
         public async Task<IActionResult> Update(MedicineUpdateDto medicineUpdateDto)
         {
-            await _service.UpdateAsync(_mapper.Map<Medicine>(medicineUpdateDto));
+            await _medicineService.UpdateAsync(_mapper.Map<Medicine>(medicineUpdateDto));
             return CreateActionResult(CustomResponseDto<NoContentDto>.Successs(204));
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> Remove(int id)
         {
-            var medicine = await _service.GetByIdAsync(id);
-            await _service.RemoveAsyn(medicine);
+            var medicine = await _medicineService.GetByIdAsync(id);
+            await _medicineService.RemoveAsyn(medicine);
             return CreateActionResult(CustomResponseDto<NoContentDto>.Successs(204));
         }
     }
