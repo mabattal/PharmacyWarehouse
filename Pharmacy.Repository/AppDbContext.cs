0d011ca935260550f8f7db9d57795d9264f343ec
mabattal <mehmetalibattal@gmail.com>
2023-11-20 13:58:25 +0300

    AppDbContext

---
+++ Pharmacy.Repository/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pharmacy.Core.Models;
+
+namespace Pharmacy.Repository
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Suplier> Categories { get; set; }
+        public DbSet<Medicine> Products { get; set; }
+    }
+}
