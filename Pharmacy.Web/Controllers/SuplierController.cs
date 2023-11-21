1fccb67d9433d43d0f885bb48b9629f940dbb497
mabattal <mehmetalibattal@gmail.com>
2023-11-21 21:04:53 +0300

    SuplierController1 - Index

---
+++ Pharmacy.Web/Controllers/SuplierController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pharmacy.Web.Services;
+
+namespace Pharmacy.Web.Controllers
+{
+    public class SuplierController : Controller
+    {
+        private readonly SuplierApiService _suplierApiService;
+
+        public SuplierController(SuplierApiService suplierApiService)
+        {
+            _suplierApiService = suplierApiService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _suplierApiService.GetAllAsync());
+        }
+
+
+    }
+}
