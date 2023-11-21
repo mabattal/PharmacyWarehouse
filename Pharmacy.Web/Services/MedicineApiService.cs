cb40b648bedaf4671d152c3bbcb420f24d5ecf2d
mabattal <mehmetalibattal@gmail.com>
2023-11-21 15:00:39 +0300

    MedicineApiServiceMethods

--- Pharmacy.Web/Services/MedicineApiService.cs
+++ Pharmacy.Web/Services/MedicineApiService.cs
@@ -1,4 +1,6 @@
-﻿namespace Pharmacy.Web.Services
+﻿using Pharmacy.Core.DTOs;
+
+namespace Pharmacy.Web.Services
 {
     public class MedicineApiService
     {
@@ -7,6 +9,46 @@
         public MedicineApiService(HttpClient httpClient)
         {
             _httpClient = httpClient;
+        }
+
+        public async Task<List<MedicineWithSuplierDto>> GetMedicinesWithSuplierAsync()
+        {
+            var response = await _httpClient.GetFromJsonAsync<CustomResponseDto<List<MedicineWithSuplierDto>>>("medicines/GetMedicinesWithSuplier");
+
+            return response.Data;
+        }
+
+        public async Task<MedicineDto> SaveAsync(MedicineDto newMedicine)
+        {
+            var response = await _httpClient.PostAsJsonAsync("medicines", newMedicine);
+
+            if (!response.IsSuccessStatusCode) return null;
+
+            var responseBody = await response.Content.ReadFromJsonAsync<CustomResponseDto<MedicineDto>>();
+
+            return responseBody.Data;
+
+        }
+
+        public async Task<bool> UpdateAsync(MedicineDto medicine)
+        {
+            var response = await _httpClient.PutAsJsonAsync("medicines", medicine);
+
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<MedicineDto> GetByIdAsync(int id)
+        {
+            var response = await _httpClient.GetFromJsonAsync<CustomResponseDto<MedicineDto>>($"medicines/{id}");
+
+            return response.Data;
+        }
+
+        public async Task<bool> RemoveAsync(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"medicines/{id}");
+
+            return response.IsSuccessStatusCode;
         }
     }
 }
